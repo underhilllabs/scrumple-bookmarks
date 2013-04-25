13e4a2d99930400578d5d0c7bfca31b4de634bb9
Bart Lantz <underhilllabs@gmail.com>
2013-04-25 11:45:09 -0400

    added models and started building bookmarks controllers and routes

---
+++ application/models/tag.php
@@ -0,0 +1,12 @@
+<?php
+
+class Tag extends Eloquent
+{
+    protected $guarded = array();
+
+    public static $rules = array();
+    public static $timestamps = true;
+    public function bookmarks() {
+      return $this->has_many('Bookmark');
+    }
+}
