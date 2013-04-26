29cb263945f6b7403dee14e84021367bc8fc9456
Bart Lantz <underhilllabs@gmail.com>
2013-04-26 12:32:30 -0400

    updated pagination and navbar

--- application/views/bookmark/index.blade.php
+++ application/views/bookmark/index.blade.php
@@ -1,9 +1,19 @@
 <html>
 <head>
-<title>L3 Bookmarks!</title>
+<title>Scrumple Bookmarks</title>
  {{ Asset::styles() }}
 </head>
 <body>
+<div class="navbar">
+  <div class="navbar-inner">
+    <a class="brand" href="#">Scrumple Bookmarks</a>
+    <ul class="nav">
+      <li class="active"><a href="/">bookmarks</a></li>
+      <li><a href="/users/">users</a></li>
+      <li><a href="/tags/">tags</a></li>
+    </ul>
+  </div>
+</div>
 <h1>Let's check out some bookmarks</h1>
 @foreach ($bookmarks->results as $bookmark)
   <div class="bookmark">
@@ -17,5 +27,7 @@
 @endforeach
 
 <span>
+<div class="pagination-centered">
 {{ $bookmarks->links() }}
+</div>
 </span>
