4e003bc80cd6bf42dfd94e5d780d19e6f337af40
Bart Lantz <underhilllabs@gmail.com>
2013-04-28 14:08:36 -0400

    updated master view to show diff navbar if user logged in

--- application/views/site/master.blade.php
+++ application/views/site/master.blade.php
@@ -13,7 +13,12 @@
       <li><a href="/tags/">tags</a></li>
     </ul>
     <ul class="nav pull-right">
+    @if(Auth::user())
+      <li><a href="/user/{{Auth::user()->id}}/bookmarks">{{Auth::user()->username}}</a></li>
+      <li><a href="/user/logout">log out</a>
+    @else
       <li><a href="/user/login">log in</a>
+    @endif
     </ul>
   </div>
 </div>
