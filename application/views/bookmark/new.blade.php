27da6dacc1b042866082f4dab9cf1bf455989a60
Bart Lantz <underhilllabs@gmail.com>
2013-04-29 17:37:00 -0400

    added the new bookmark form

--- application/views/bookmark/new.blade.php
+++ application/views/bookmark/new.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 Welcome, {{Auth::user()->username}}
+{{Form::open()}}
+{{Form::token()}}
 
+{{Form::label('address', 'Address')}}
+{{Form::text('address', Input::get('address'))}}
+
+{{Form::label('title', 'Title')}}
+{{Form::text('title', Input::get('title'))}}
+
+{{Form::label('desc', 'Description')}}
+{{Form::text('desc', Input::get('desc'))}}
+
+{{Form::label('tags', 'Tags')}}
+{{Form::text('tags', Input::get('tags'))}}
+<br />
+{{Form::submit('Add Bookmark')}}
+{{Form::close()}}
 @endsection
 
