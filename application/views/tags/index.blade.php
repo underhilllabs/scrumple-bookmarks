8241c6f99629666e5a3f92fb0cb1ed63fe79511e
Bart Lantz <underhilllabs@gmail.com>
2013-05-01 14:46:32 -0400

    updating tag/name link

--- application/views/tags/index.blade.php
+++ application/views/tags/index.blade.php
@@ -1,12 +1,11 @@
 @layout('site.master')
 
 @section('content')
-  <h1>Let's check out some bookmarks</h1>
   <div class="row-fluid">
     <div class="tags span8">
     @foreach ($tags as $tag)
       @if ($tag->count >= 10) 
-        <span class="tag"><a href="/tags/{{ $tag->name }}">{{ $tag->name }}</a>({{$tag->count}})</span>
+        <span class="tag"><a href="/tag/{{ $tag->name }}">{{ $tag->name }}</a>({{$tag->count}})</span>
       @endif
     @endforeach
     </div>
