5336a5346058cc05a2be9978c78412bdce674d02
Bart Lantz <underhilllabs@gmail.com>
2013-05-01 01:17:49 -0400

    adjusted date output on bookmark stream

--- application/views/bookmark/index.blade.php
+++ application/views/bookmark/index.blade.php
@@ -1,11 +1,10 @@
 @layout('site.master')
 
 @section('content')
-<h1>Let's check out some bookmarks</h1>
 @foreach ($bookmarks->results as $bookmark)
   <div class="bookmark">
     <a href="{{$bookmark->url}}">{{$bookmark->title}}</a><br />
-    {{ $bookmark->created_at }} to <span>
+    {{ preg_split("/ /",$bookmark->created_at)[0] }} to <span>
     @foreach ($bookmark->tags as $tag) 
       <a href="/tags/{{ $tag->name }}">{{ $tag->name }}</a>
     @endforeach
