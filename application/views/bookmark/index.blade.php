535ba76d9bfed88e9bb77e6ad4df728110a1f5f5
Bart Lantz <underhilllabs@gmail.com>
2013-07-28 01:34:15 -0400

    adding tags div to front page

--- application/views/bookmark/index.blade.php
+++ application/views/bookmark/index.blade.php
@@ -1,24 +1,32 @@
 @layout('site.master')
 
 @section('content')
+<div class="span12">
 @foreach ($bookmarks->results as $bookmark)
   <div class="bookmark">
     <a href="{{$bookmark->url}}">{{$bookmark->title}}</a><br />
-    {{ preg_split("/ /",$bookmark->created_at)[0] }} <span>
-    @if ($bookmark->tags)
-      to
-    @endif
-    @foreach ($bookmark->tags as $tag) 
-      <a href="/tag/{{ $tag->name }}">{{ $tag->name }}</a>
-    @endforeach
-  by <a href="/user/{{$bookmark->user_id}}/bookmarks/">{{ $bookmark->user()->first()->username }}</a>
-    @if (Auth::user() && $bookmark->user_id == Auth::user()->id)
-      - <a href="/bookmarks/{{$bookmark->id}}/edit">edit</a>
-      - <a href="/bookmarks/{{$bookmark->id}}/delete">delete</a>
-    @endif
+    {{ preg_split("/ /",$bookmark->created_at)[0] }} 
+    <span>
+      @if ($bookmark->tags)
+        to
+      @endif
+      @foreach ($bookmark->tags as $tag) 
+        <a href="/tag/{{ $tag->name }}">{{ $tag->name }}</a>
+      @endforeach
+    by <a href="/user/{{$bookmark->user_id}}/bookmarks/">{{ $bookmark->user()->first()->username }}</a>
+      @if (Auth::user() && $bookmark->user_id == Auth::user()->id)
+        - <a href="/bookmarks/{{$bookmark->id}}/edit">edit</a>
+        - <a href="/bookmarks/{{$bookmark->id}}/delete">delete</a>
+      @endif
     </span>
   </div>
 @endforeach
+</div>
+
+<div class="span3" id="taglist">
+</div>
+
+<br clear="all" />
 
 <span>
 <div class="pagination-centered">
